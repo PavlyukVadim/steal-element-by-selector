c6b4f3962c91dd19f2aa5117c917d163f2e89ce7
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-02-17 22:16:24 +0200

    initial request

---
+++ index.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+let options = {
+	host: 'google.com',
+  	method: 'GET'
+}
+
+var req = http.request(options, (res) => {
+  console.log(`STATUS: ${res.statusCode}`);
+  console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+  res.setEncoding('utf8');
+  res.on('data', (chunk) => {
+    console.log(`BODY: ${chunk}`);
+  });
+  res.on('end', () => {
+    console.log('No more data in response.');
+  });
+});
+
+req.end();
