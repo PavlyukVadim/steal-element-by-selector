2321d5d4e6fece5b244cfd216e50267e7693bdc7
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-02-22 23:55:04 +0200

    add parse url, http/https requests

--- index.js
+++ index.js
@@ -1,20 +1,54 @@
-const http = require('http');
+'use strict';
 
-let options = {
-	host: 'google.com',
-  	method: 'GET'
+const url = require('url');
+const http = require('http');
+const https = require('https');
+
+let href = 'https://www.google.com.ua';
+
+function getCodeByHref (href) {
+
+  let urlObj = url.parse(href);
+  let options = {
+    hostname: urlObj.hostname,
+    port: 443,
+    path: urlObj.path,
+    method: 'GET'
+  };
+
+  if (urlObj.protocol === 'https:') {
+    httpsRequest(options);
+  } else if (urlObj.protocol === 'http:') {
+    httpRequest(options);
+  } 
+  
 }
 
-var req = http.request(options, (res) => {
-  console.log(`STATUS: ${res.statusCode}`);
-  console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-  res.setEncoding('utf8');
-  res.on('data', (chunk) => {
-    console.log(`BODY: ${chunk}`);
-  });
-  res.on('end', () => {
-    console.log('No more data in response.');
-  });
-});
+getCodeByHref("https://www.google.com.ua");
 
-req.end();
+function httpRequest(options) {
+  var req = http.request(options, (res) => {
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+      console.log(`BODY: ${chunk}`);
+    });
+    res.on('end', () => {
+      console.log('No more data in response.');
+    });
+  });
+
+  req.end();
+}
+
+function httpsRequest(options) {
+  var req = https.request(options, (res) => {
+    res.on('data', (d) => {
+      process.stdout.write(d);
+    });
+  });
+
+  req.on('error', (e) => {
+    console.error(e);
+  });
+  req.end();
+}
