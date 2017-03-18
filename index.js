dfe3249b94f96e49d57fb914b5ff071d1bc6134c
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-03-18 16:14:52 +0200

    remove httpRequest

--- index.js
+++ index.js
@@ -1,66 +1,47 @@
 'use strict';
 
 const url = require('url');
-const httpsRequest = require('./app/httpsRequest');
-const httpRequest = require('./app/httpRequest');
 const cheerio = require('cheerio');
-
+const requestPromise = require('request-promise');
 
 const href = 'https://luxoptica.ua/';
 const cssHref = 'http://sqdev.cc/node_modules/bulma/css/bulma.css';
 const selector = 'img';
-const requestPromise = require('request-promise');
 
 
-function request() {
-  var options = {
-    uri: 'https://luxoptica.ua/',
+
+function getCssLinks(siteLink) {
+  const options = {
+    uri: siteLink,
     transform: function (body) {
-        return cheerio.load(body);
+      return cheerio.load(body);
     }
   };
- 
+  
   requestPromise(options)
-    .then(($) => findHrefs($))
+    .then(($) => {
+        let cssLinks = $('link').toArray()
+          .filter(link => link.attribs.type === 'text/css')
+          .map(link => link.attribs.href);
+          console.log( cssLinks );
+        return cssLinks;  
+    })
+    .catch((reason) => console.log('Reason:', reason));  
+}
+getCssLinks(href);
+
+function getCssByHref (cssLink, selector) {
+  const options = {
+    uri: cssLink
+  }
+
+  requestPromise(options)
+    .then((body) => ('}' + body))
+    .then((body) => parseCss(body, selector))
     .catch((reason) => console.log('Reason:', reason));
 }
 
-request();
-
-
-
-function findHrefs($) {
-  let cssLinks = $('link').toArray()
-    .filter(link => link.attribs.type === 'text/css')
-    .map(link => link.attribs.href);
-  console.log( cssLinks );
-  //console.log( $('head').html() );
-}
-
-function getCssByHref (href, selector) {
-
-  let urlObj = url.parse(href);
-  let options = {
-    hostname: urlObj.hostname,
-    port: 80,
-    path: urlObj.path,
-    method: 'GET'
-  };
-  let bodyRequest;
-  
-  if (urlObj.protocol === 'https:') {  
-    bodyRequest = httpsRequest;
-  } else if (urlObj.protocol === 'http:') {
-    bodyRequest = httpRequest;
-  }
-  
-  bodyRequest(options)
-  .then((body) => ('}' + body))
-  .then((body) => parseCss(body, selector))
-  .catch((reason) => console.log('Reason:', reason));
-}
-
-//getCssByHref(cssHref, selector);
+getCssByHref(cssHref, selector);
 
 
 function parseCss(body, basicSelector) {
